4eb3c11c46f4c1b10c76deea36361a779834525e
chain-smok <101109105+chain-smok@users.noreply.github.com>
2022-11-24 16:49:13 +0800

    Add files via upload

---
+++ 11.24JS/1124.js
@@ -0,0 +1,19 @@
+var fruits=[];
+
+function Hello()
+{
+  
+  var name;
+  name=prompt("please enter your name");
+  //alert("Hello Welcomne,my Website "+name);
+
+  node1=document.getElementById("test");
+  node1.innerHTML="Welcome "+name;
+  node1.style.backgroundColor="green";
+}
+//document.getElementById("test").innerHTML="HELLO TEST";
+
+function initial(){
+
+
+}  
